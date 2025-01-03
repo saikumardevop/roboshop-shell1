d830e993bd9c0331aabb3207ec186caa4a88a262
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-03 18:53:09 +0530

    mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,10 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum  install mongodb-org -y
 
+sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
 systemctl enable mongod
 systemctl start mongod
 
-# Edit the file the  replace 127.0.0.1 with 0.0.0.0
+
 
