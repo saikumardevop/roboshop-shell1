848582e860cddb8dafeaffc3a1830bf8ba136a65
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-08 18:19:50 +0530

    cart

--- cart.sh
+++ cart.sh
@@ -20,10 +20,10 @@ unzip /tmp/cart.zip
 
 echo -e "\e[33m>>>>>>>>> Install   NodeJS  Dependencies <<<<<<<<\e[0m"
 npm install
-echo -e "\e[33m>>> Copy Catalogue SystemD file <<<<<<<<\e[0m"
+echo -e "\e[33m>>> Copy Cart SystemD file <<<<<<<<\e[0m"
 cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
 
-echo -e "\e[33m>>> Start  User Service <<<<<<<<\e[0m"
+echo -e "\e[33m>>> Start  cart Service <<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart ; tail /var/log/messages
