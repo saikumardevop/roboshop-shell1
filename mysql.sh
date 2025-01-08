b1308f285e8c5e2922a5d6b4769c74099d2506bf
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-08 19:36:54 +0530

    cart

---
+++ mysql.sh
@@ -0,0 +1,15 @@
+echo -e "\e[33m>>>>>>>>>  Disable Mysql 8 version  <<<<<<<<\e[0m"
+dnf module disable mysql -y
+
+echo -e "\e[33m>>>>>>>>>  Copy Mysql Repo File  <<<<<<<<\e[0m"
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "\e[33m>>>>>>>>> Installed Mysql <<<<<<<<\e[0m"
+dnf install mysql-community-server -y
+
+echo -e "\e[33m>>> Start MYSQL <<<<<<<<\e[0m"
+systemctl enable mysql
+systemctl start cart
+
+echo -e "\e[33m>>> Reset MYSQL <<<<<<<<\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
