b21562eac15eb7c49cf9d830966f0210d2feeee7
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-29 22:36:26 +0530

    source common

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ print_head(){
 }
 
 schema_setup(){
- if [" $ schema_setup" == "mongo" ] ; then
+ if [ "$ schema_setup" == "mongo" ] ; then
    print_head "Copy MongoDB repo"
    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
