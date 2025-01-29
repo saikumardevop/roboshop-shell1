4334f063b1f31ac837045e4a45a6fca6448294d4
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-29 13:40:15 +0530

    COMMON.SH

--- mysql.sh
+++ mysql.sh
@@ -5,14 +5,14 @@ echo -e "\e[33m>>>>>>>>>  Disable Mysql 8 version  <<<<<<<<\e[0m"
 dnf module disable mysql -y
 
 echo -e "\e[33m>>>>>>>>>  Copy Mysql Repo File  <<<<<<<<\e[0m"
-cp ${script_path}/mysql.repo /etc/systemd/system/mysql.repo
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
 
 echo -e "\e[33m>>>>>>>>> Installed Mysql <<<<<<<<\e[0m"
 dnf install mysql-community-server -y
 
 echo -e "\e[33m>>> Start MYSQL <<<<<<<<\e[0m"
 systemctl enable mysqld
-systemctl restart mysqld
+systemctl start mysqld
 
 echo -e "\e[33m>>> Reset MYSQL  Password <<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
