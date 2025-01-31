ef05ad21872f565c1f1884b69ddc5f02a78a8641
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-31 17:28:40 +0530

    shipping changes

--- common.sh
+++ common.sh
@@ -5,24 +5,37 @@ script_path=$(dirname "$script")
 func_print_head(){
   echo -e "\e[36m >>>>>>>> $* <<<<<<<<<<<<\e[0m"
 }
+func_stat_check (){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[33mFALIURE\e[0m"
+    exit 1
+  fi
+}
 
 func_schema_setup(){
  if [ "$ schema_setup" == "mongo" ] ; then
    func_print_head "Copy MongoDB repo"
    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+   func_stat_check $?
 
    func_print_head Install  "MongoDB Client"
    dnf install mongodb-org-shell -y
+   func_stat_check $?
 
    func_print_head "Load Schema "
    mongo --host mongodb-dev.rajasekhar72.store </app/schema/${component}
+   func_stat_check $?
  fi
  if [ "${schema_setup}" == "mysql" ]; then
    func_print_head  "INSTALL MYSQL client"
    dnf install mysql -y
+   func_stat_check $?
 
    func_print_head  "Load Schema"
    mysql -h mysql-dev.rajasekhar72.store  -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+   func_stat_check $?
  fi
 
 }
@@ -30,40 +43,49 @@ func_schema_setup(){
 func_app_prereq(){
   func_print_head "Create Application User"
   useradd ${app_user}
+  func_stat_check $?
 
   func_print_head "Create Application Directory"
   rm -rf /app
   mkdir /app
+  func_stat_check $?
 
   func_print_head "Download Application Content"
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  func_stat_check $?
 
   func_print_head "Extract Application Content"
   cd /app
   unzip /tmp/${component}.zip
+  func_stat_check $?
 }
 
 func_systemd_setup(){
   func_print_head "Setup Systemd Service"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  func_stat_check $?
 
   func_print_head "Start  ${component} Service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
+  func_stat_check $?
 
 }
 func_nodejs(){
   func_print_head  "configuring nodejs  repos"
   curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
+  func_stat_check $?
 
   func_print_head  "Install Nodejs  repos"
   dnf install nodejs -y
+  func_stat_check $?
 
   func_app_prereq
 
   func_print_head  "Install   NodeJS  Dependencies"
   npm install
+  func_stat_check $?
 
   func_schema_setup
   func_systemd_setup
@@ -73,22 +95,14 @@ func_nodejs(){
 func_java(){
   func_print_head "Install Maven"
   yum install maven -y
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[33mFALIURE\e[0m"
-  fi
+  func_stat_check $?
+
 
   func_app_prereq
 
   func_print_head "Download Maven Dependencies "
   mvn clean package
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[33mFALIURE\e[0m"
-  fi
-
+  func_stat_check $?
   mv target/${component}-1.0.jar ${component}.jar
 
   func_schema_setup
