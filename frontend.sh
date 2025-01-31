1f41bc2971515388952b7f127364507de7f69434
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-31 23:07:36 +0530

    shipping changes

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,33 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl enable nginx
-systemctl restart nginx
+
+func_print_head "Download Maven Dependencies"
+mvn clean package &>>$logfile
+func_stat_check $?
+
+func_print_head
+yum install nginx -y &>>$log_file
+func_stat_check $?
+
+func_print_head
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_stat_check $?
+
+func_print_head
+rm -rf /usr/share/nginx/html/* &>>$log_file
+func_stat_check $?
+
+func_print_head
+curl -o/tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+func_stat_check $?
+
+func_print_head
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+func_stat_check $?
+
+func_print_head
+sistemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+func_stat_check $?
