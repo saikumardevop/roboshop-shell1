a592be7777ec4d0956fb0eebd26e8c0fc85ce575
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-31 19:45:41 +0530

    shipping changes

--- common.sh
+++ common.sh
@@ -46,7 +46,11 @@ func_schema_setup(){
 
 func_app_prereq(){
   func_print_head "Create Application User"
-  useradd ${app_user} &>>$log_file &>>$log_file
+  id ${app_user} &>>/tmp/roboshop.log
+  if [ $? -ne 0 ]; then
+  useradd ${app_user} &>>/tmp/roboshop.log
+  fi
+  func_stat_check $?
   func_stat_check $?
 
   func_print_head "Create Application Directory"
