60127901ae08f179f4005800832f8b77372d1ea6
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-01-31 23:48:41 +0530

    shipping changes

--- mysql.sh
+++ mysql.sh
@@ -3,24 +3,29 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 
-if{ -z "mysql_root_password"};then
+if [ -z "mysql_root_password" ]; then
 echo Input Roboshop Appuser password Missing
 exit
 fi
 
-echo -e "\e[33m>>>>>>>>>  Disable Mysql 8 version  <<<<<<<<\e[0m"
-dnf module disable mysql -y
+func_print_head "  Disable Mysql 8 version"
+dnf module disable mysql -y &>>$log_file
+func_stat_check $?
 
-echo -e "\e[33m>>>>>>>>>  Copy Mysql Repo File  <<<<<<<<\e[0m"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
+func_print_head "  Copy Mysql Repo File"
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+func_stat_check $?
 
-echo -e "\e[33m>>>>>>>>> Installed Mysql <<<<<<<<\e[0m"
-dnf install mysql-community-server -y
+func_print_head " Installed Mysql"
+dnf install mysql-community-server -y &>>$log_file
+func_stat_check $?
 
-echo -e "\e[33m>>> Start MYSQL <<<<<<<<\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+func_print_head "Start MYSQL"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+func_stat_check $?
 
-echo -e "\e[33m>>> Reset MYSQL  Password <<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass $mysql_root_password
+func_print_head "Reset MYSQL  Password"
+mysql_secure_installation --set-root-pass $mysql_root_password &>>$log_file
+func_stat_check $?
 
