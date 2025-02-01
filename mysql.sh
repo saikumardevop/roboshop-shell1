b0c66a47ed21e06a89fe545c30d655926acabde5
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-01 11:55:20 +0530

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,6 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
-log_file=/tmp/roboshop.log
 
 if [ -z "$mysql_root_password" ]; then
   echo Input MySQL Root Password Missing
@@ -10,22 +9,22 @@ if [ -z "$mysql_root_password" ]; then
 fi
 
 func_print_head "Disable MySQL 8 Version"
-dnf module disable mysql -y  &>>$log_file
+dnf module disable mysql -y
 func_stat_check $?
 
 func_print_head "Copy MySQL Repo File"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
 func_stat_check $?
 
 func_print_head "Install MySQL"
-yum install mysql-community-server -y &>>$log_file
+yum install mysql-community-server -y
 func_stat_check $?
 
 func_print_head "Start MySQL"
-systemctl enable mysqld &>>$log_file
-systemctl restart mysqld &>>$log_file
+systemctl enable mysqld
+systemctl restart mysqld
 func_stat_check $?
 
 func_print_head "Reset MySQL Password"
-mysql_secure_installation--set-root-pass $mysql_root_password &>>$log_file
+mysql_secure_installation--set-root-pass $mysql_root_password
 func_stat_check $?
