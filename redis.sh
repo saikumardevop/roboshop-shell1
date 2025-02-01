a9b09651cd76301c23856d9a968710fdb09baed2
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-01 10:51:57 +0530

    shipping changes

--- redis.sh
+++ redis.sh
@@ -12,7 +12,7 @@ yum install redis -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Update Redis Listen Address"
-sed -i -e 's|127.0.0.1|0.0.0.0|'etc/redis.conf /etc/redis/redis.conf &>>$log_file
+sed -i -e 's|127.0.0.1|0.0.0.0|' etc/redis.conf /etc/redis/redis.conf &>>$log_file
 func_stat_check $?
 
 func_print_head "Start Redis Service"
