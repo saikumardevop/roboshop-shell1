e5e95ad930a050c084f754b8f53ea2a0269e8dea
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-01 11:42:34 +0530

    shipping changes

--- mysql.sh
+++ mysql.sh
@@ -14,7 +14,7 @@ dnf module disable mysql -y  &>>$log_file
 func_stat_check $?
 
 func_print_head "Copy MySQL Repo File"
-cp ${scrip_path}/mysql.repo/etc/yum.repos.d/mysql.repo &>>$log_file
+cp ${script_path}/mysql.repo/etc/yum.repos.d/mysql.repo &>>$log_file
 func_stat_check $?
 
 func_print_head "Install MySQL"
