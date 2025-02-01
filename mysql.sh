735811115bbd61728e18ff1e656063fb423e02c6
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-01 12:24:20 +0530

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -9,22 +9,22 @@ if [ -z "$mysql_root_password" ]; then
 fi
 
 func_print_head "Disable MySQL 8 Version"
-dnf module disable mysql -y
+dnf module disable mysql -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Copy MySQL Repo File"
-cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo
+cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 func_stat_check $?
 
 func_print_head "Install MySQL"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Start MySQL"
-systemctl enable mysqld
-systemctl restart mysqld
+systemctl enable mysqld &>>$log_file
+systemctl restart mysqld &>>$log_file
 func_stat_check $?
 
 func_print_head "Reset MySQL Password"
-mysql_secure_installation--set-root-pass $mysql_root_password
+mysql_secure_installation--set-root-pass $mysql_root_password &>>$log_file
 func_stat_check $?
