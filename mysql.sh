e8ffd3ebcea13008107cdab08601c700bd0ebef1
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-01 12:30:18 +0530

    mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -9,6 +9,7 @@ func_print_head(){
   echo -e "\e[36m >>>>>>>> $* <<<<"
   echo -e "\e[36m >>>>>>>> $* <<<<" &>>$log_file
 }
+
 func_stat_check (){
   if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
